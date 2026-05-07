377b67e64ec66c355dd09bf2e3852b2e1de6216f
MAVI x. <morganheger33@gmail.com>
2026-05-07 16:01:14 +0500

    Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,32 +1,32 @@
 
 #write a progam that sum two numbers
-#value1 = 79
-#value2 = 4
-#sum = value1 + value2
-#print("the final result is:",sum)
+value1 = 79
+value2 = 4
+sum = value1 + value2
+print("the final result is:",sum)
 
 #Write a program that multiply two numbers
-#digit1 = 9
-#digit2 = 7
-#multiply = digit1 * digit2
-#print("the final result is:", multiply)
+digit1 = 9
+digit2 = 7
+multiply = digit1 * digit2
+print("the final result is:", multiply)
 
 #Retrying as a correction
-#print("Ha" * 3)
+print("Ha" * 3)
 
-#print(0 * 4)
+print(0 * 4)
 
-#print([0] * 4)
+print([0] * 4)
 
-#digit1 = 17
-#digit2 = 19
-#print(digit1 * digit2)
+digit1 = 17
+digit2 = 19
+print(digit1 * digit2)
 
 
 #Write a program that divides two numbers
 Value1 = 25
 Value2 = 5
-#print(25 / 5)
+print(25 / 5)
 
 
 #Write a program that gives Modulus
