76a164f234b3eb8a2866caf15dee7361d2e4066f
morganheger33-creator <morganheger33@gmail.com>
2026-05-11 15:30:43 +0500

    Assignment2 Added

---
+++ Assignment2.py
@@ -0,0 +1,34 @@
+#Write a program that multiplies two variables 
+var1 = int(input("enter the first number"))
+var2 = int(input("enter the second number"))
+mul = var1 * var2
+print("The final result is:", mul) 
+
+
+#Write a program that subtracts two varibales
+Var1 = int(input("enter the first number"))
+Var2 = int(input("enter the second number"))
+Subtract = Var1 - Var2 
+print("The final result is:", Subtract)
+
+
+#Write a program that Divides two variables
+Val1 = int(input("enter the first number"))
+Val2 = int(input("enter the second number"))
+Div = Val1 / Val2 
+print("The final result is:", Div)
+
+
+#Write a program that sums two variables 
+Amt1 = int(input("enter the first number"))
+Amt2 = int(input("enter the second number"))
+Sum = Amt1 + Amt2 
+print("The final result is:", Sum) 
+
+
+
+#Write a program that modules two variables
+Dig1 = int(input("enter the first number"))
+Dig2 = int(input("enter the second number"))
+Mod = Dig1 % Dig2
+print("The final result is:", Mod)
