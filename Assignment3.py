8fee850ad61fad2618e888d33937b2fb7dbed51e
morganheger33-creator <morganheger33@gmail.com>
2026-05-13 15:53:24 +0500

    Conditional expressions

---
+++ Assignment3.py
@@ -0,0 +1,7 @@
+#Write a program that defines Conditional Expressions
+a =int(input("Enter your obtained marks: "))
+b = 65
+if a >= b:
+  print("Congrats Bud, You've Passed.")
+else:
+  print("Sorry, Better luck next time.")
