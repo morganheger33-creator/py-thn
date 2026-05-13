56d1b8084fb7bddaa9d24c40978721da85e2ebf6
morganheger33-creator <morganheger33@gmail.com>
2026-05-13 16:02:58 +0500

    Another expression added

--- Assignment3.py
+++ Assignment3.py
@@ -1,7 +1,18 @@
 #Write a program that defines Conditional Expressions
-a =int(input("Enter your obtained marks: "))
-b = 65
-if a >= b:
-  print("Congrats Bud, You've Passed.")
+#a =int(input("Enter your obtained marks: "))
+#b = 65
+#if a >= b:
+  #print("Congrats Bud, You've Passed.")
+#else:
+  #print("Sorry, Better luck next time.")
+
+
+  #Write another program that shows Conditional statement
+a = int(input("Enter first value: "))
+b = int(input("Enter second value: "))
+if a>b:
+    print("First value is greater than second one")
+elif a<b:
+    print("First value is smaller than second one")
 else:
-  print("Sorry, Better luck next time.")
+    print("Both vlaues are equal")
