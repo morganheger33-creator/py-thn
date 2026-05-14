3b9eec13bf4ebab0c442416001c5a9fbd9d5ddbc
morganheger33-creator <morganheger33@gmail.com>
2026-05-14 14:37:11 +0500

    Assignment added

---
+++ Assigment4.py
@@ -0,0 +1,18 @@
+#Write a program that checks if a number is positive/negative or Zero
+num = float(input("Enter a number: "))
+if num > 0:
+    print("The number is positive")
+elif num == 0:
+    print("The number is Zero")
+else: 
+    print("The number is negative") 
+
+
+# Python program to check if the input number is odd or even.
+# A number is even if division by 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+    print("{0} is Even".format(num))
+else:
+    print("{0} is Odd".format(num))
