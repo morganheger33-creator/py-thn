38602d898b70c2120fda723603ba7ae6521144c4
morganheger33-creator <morganheger33@gmail.com>
2026-05-21 14:56:28 +0500

    Another example added

--- Assignment5.py
+++ Assignment5.py
@@ -21,3 +21,24 @@ elif day_no == "7" :
     print("Weekend or Sunday")
 else : 
     print("Please enter correct day number between 1-7.")
+
+
+
+#Using conditional statements to know temperature 
+#If temperature is less than -5 very cold, lower than 5 cold,
+#lower than 15 cool, lower than 30 normal, above 30 warm, above 40 very hot.
+
+temperature = int(input("Enter temperature."))
+if temperature > 40 : #Above 40 
+    print("Temperature is very hot")
+elif temperature > 30 : #31 - 40
+    print("Temperature is warm")
+elif temperature > 15 : #16 - 30
+    print("Temperature is normal")
+elif temperature > 5 : #6 - 15
+    print("Temperature is cool")
+elif temperature > -4: #-3 - 5
+    print("Temperature is cold")
+else:
+    print("Temperature is freezing") #Lower than -4
+    
