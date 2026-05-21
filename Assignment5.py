21e86e7e13b4a3336bda87087bbefeea439d3ef2
morganheger33-creator <morganheger33@gmail.com>
2026-05-21 14:37:16 +0500

    Assigment added

---
+++ Assignment5.py
@@ -0,0 +1,23 @@
+#Multi conditional statements 
+#If 
+#If else
+#If elif else
+
+#Enter day no and show the day
+day_no = input("Enter day number.")
+if day_no == "1" :
+    print("The day is Monday")
+elif day_no == "2" :
+    print("The day is Tuesday")
+elif day_no == "3" :
+    print("The day is Wednesday")
+elif day_no == "4" :
+    print("The day is Thursday")
+elif day_no == "5" :
+    print("The day is Friday")
+elif day_no == "6" :
+    print("The day is Saturday")
+elif day_no == "7" :
+    print("Weekend or Sunday")
+else : 
+    print("Please enter correct day number between 1-7.")
