5a24addca4745f05ddfbeaa5d833d344799302d2
morganheger33-creator <morganheger33@gmail.com>
2026-05-25 16:37:26 +0500

    Assignment added

---
+++ Assignment7.py
@@ -0,0 +1,7 @@
+#Writing a program to understand the concept of looping (using "for")
+var = 0 
+for part in "We are sitting here and also waiting for the Eid ul Adha festival":
+    print(part)
+    var += 1 
+print(var)
+
